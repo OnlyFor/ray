4e79822454f80f38bed8e4731b97891f45235066
can <can@anyscale.com>
2024-06-11 04:29:03 +0000

    [doc][api/7] handle ~ character in autodoc
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/doc/test_autodoc.py
+++ ci/ray_ci/doc/test_autodoc.py
@@ -4,7 +4,7 @@ import sys
 import pytest
 
 from ci.ray_ci.doc.autodoc import Autodoc
-from ci.ray_ci.doc.mock.mock_module import MockClass, mock_function
+from ci.ray_ci.doc.mock.mock_module import MockClass, mock_function, mock_w00t
 from ci.ray_ci.doc.api import API, AnnotationType, CodeType
 
 
@@ -15,9 +15,10 @@ def test_walk():
             f.write("\tapi_01.rst\n")
             f.write("\tapi_02.rst\n")
         with open(os.path.join(tmp, "api_01.rst"), "w") as f:
-            f.write(".. currentmodule:: ci.ray_ci.doc.mock\n")
+            f.write(".. currentmodule:: ci.ray_ci.doc\n")
             f.write(".. autosummary::\n\n")
-            f.write("\tmock_function\n")
+            f.write("\t~mock.mock_function\n")
+            f.write("\tmock.mock_module.mock_w00t\n")
         with open(os.path.join(tmp, "api_02.rst"), "w") as f:
             f.write(".. currentmodule:: ci.ray_ci.doc.mock\n")
             f.write(".. autoclass:: MockClass\n")
@@ -28,6 +29,11 @@ def test_walk():
             [
                 API(
                     name=f"{mock_function.__module__}.{mock_function.__qualname__}",
+                    annotation_type=AnnotationType.PUBLIC_API,
+                    code_type=CodeType.FUNCTION,
+                ),
+                API(
+                    name=f"{mock_w00t.__module__}.{mock_w00t.__qualname__}",
                     annotation_type=AnnotationType.PUBLIC_API,
                     code_type=CodeType.FUNCTION,
                 ),
